ef232fb74f35fb9f639eb9f517f6ab7723a4817e
tohich51 <tohich51@gmail.com>
2022-10-24 12:00:14 +0300

    Update run.sh

--- hw1/run.sh
+++ hw1/run.sh
@@ -12,6 +12,15 @@ yarn jar $HADOOP_STREAMING_JAR \
         -combiner 'python3 combiner_mean.py'\
         -reducer 'python3 reducer_mean.py' \
         -input ${1} \
-        -output ${2}
+        -output ${2}_mean
+        
+yarn jar $HADOOP_STREAMING_JAR \
+        -files mapper_mean.py,reducer_mean.py \
+        -D mapred.job.name=${3} \
+        -mapper 'python3 mapper_std.py' \
+        -reducer 'python3 reducer_std.py' \
+        -input ${1} \
+        -output ${2}_var
 
-hdfs dfs -cat ${2}/part-00000 | head -n 50
+hdfs dfs -cat ${2}_mean/part-00000 | head -n 1
+hdfs dfs -cat ${2}_var/part-00000 | head -n 2
